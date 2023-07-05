95f5092e2b40a49ea9e35f1e5cbd7870408d9326
Mahdieh Torabi <mahdiehtorabi00@gmail.com>
2023-07-05 16:39:25 +0330

    Fix Order cmp

--- golden-rose/project/src/Components/Order/Order.js
+++ golden-rose/project/src/Components/Order/Order.js
@@ -2,35 +2,57 @@ import React from 'react';
 // COMPONENTS
 // STYLE
 import Style from "./Order.module.css";
-import { FlexWrapper } from "../StyledComponent";
+import { FlexWrapper, CustomLink } from "../StyledComponent";
 // IMG
 import Rose from "../../Images/BGRose.svg";
 import OrderImg from "../../Images/Order/Order.svg";
+import whiteArrow from "../../Images/Order/whiteArrow.svg";
+import blackArrow from "../../Images/Order/blackArrow.svg";
+import sOne from "../../Images/Order/sOne.svg";
+import sTwo from "../../Images/Order/sTwo.svg";
+import sThree from "../../Images/Order/sThree.svg";
+import sFour from "../../Images/Order/sFour.svg";
+import sFive from "../../Images/Order/sFive.svg";
 
 const Order = () => {
     return (
         <div className={Style.container}>
             <img src={Rose} alt="Rose" className={Style.bgRoseTop} />
             <FlexWrapper className={Style.orderBox}>
-                <FlexWrapper justifyContent={"center"} className={Style.orderImg}>
-                    <img src={OrderImg} />
-                </FlexWrapper>
+                <FlexWrapper justifyContent={"center"} className={Style.orderImg}><img src={OrderImg} /></FlexWrapper>
                 <div className={Style.orderSteps}>
                     <h1>آیا به یک محصول سفارشی نیاز دارید؟</h1>
                     <div className={Style.steps}>
-                        <div className={Style.item1}>1</div>
-                        <div className={Style.item2}>2</div>
-                        <div className={Style.item3}>3</div>
-                        <div className={Style.item4}>4</div>
-                        <div className={Style.item5}>5</div>
-                        <div className={Style.item6}>6</div>
-                        <div className={Style.item7}>7</div>
-                        <div className={Style.item8}>8</div>
-                        <div className={Style.item9}>9</div>
-                        <div className={Style.item10}>10</div>
-                        <div className={Style.item11}>11</div>
-                        <div className={Style.item12}>12</div>
-                    </div>
+                        <div className={Style.item1}>
+                            <img className={Style.stepIcon} src={sOne} alt='sign up' />
+                            <span>در سایت ثبت نام کنید و یا وارد شوید.</span>
+                        </div>
+                        <div className={Style.item2}><img src={whiteArrow} alt='next' /></div>
+                        <div className={Style.item3}>
+                            <img className={Style.stepIcon} src={sTwo} alt='search' />
+                            <span>محصول خود را جستجو کنید.</span>
+                        </div>
+                        <div className={Style.item4}><img src={whiteArrow} alt='next' /></div>
+                        <div className={Style.item5}>
+                            <img className={Style.stepIcon} src={sThree} alt='fill up' />
+                            <span>فرم مورد نظر را پر کنید.</span>
+                        </div>
+                        <div className={Style.item6}><img src={whiteArrow} alt='next' /></div>
+                        <div className={Style.item7}>
+                            <img className={Style.stepIcon} src={sFour} alt='choose' />
+                            <span>نوع بسته بندی خود را انتخاب کنید.</span>
+                        </div>
+                        <div className={Style.item8}><img src={whiteArrow} alt='next' /></div>
+                        <div className={Style.item9}>
+                            <img className={Style.stepIcon} src={sFive} alt='shop' />
+                            <span>خرید خود را نهایی کنید.</span>
+                        </div>
+                        <CustomLink className={Style.startWrapper}>
+                            <button className={Style.start}>
+                                شروع فرآیند
+                                <img src={blackArrow} alt='arrow' />
+                            </button>
+                        </CustomLink></div>
                 </div>
             </FlexWrapper>
             <img src={Rose} alt="Rose" className={Style.bgRoseBottom} />
