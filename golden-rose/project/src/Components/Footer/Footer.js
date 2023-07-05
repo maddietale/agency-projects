deda3fef4e985062a38849a750f0a1a884f40abf
Mahdieh Torabi <mahdiehtorabi00@gmail.com>
2023-07-05 13:29:49 +0330

    copyright Done and griding Footer cmp :)

--- golden-rose/project/src/Components/Footer/Footer.js
+++ golden-rose/project/src/Components/Footer/Footer.js
@@ -1,11 +1,18 @@
 import React from 'react';
 // STYLE
 import Style from "./Footer.module.css";
+import { CustomLink, FlexWrapper } from '../StyledComponent';
 
 const Footer = () => {
     return (
         <div className={Style.container}>
-            I'm Footer.
+            <div className={Style.footerWrapper}>
+                <div className={Style.clm1}>1</div>
+                <div className={Style.clm2}>2</div>
+                <div className={Style.clm3}>3</div>
+                <div className={Style.clm4}>4</div>
+            </div>
+            <div className={Style.copyright}>این یک طرح کانسپت از “علی محمدزاده” است و هیچ گونه استفاده تجاری ندارد. (کلیه حقوق این سایت محفوظ است.)</div>
         </div>
     );
 };
